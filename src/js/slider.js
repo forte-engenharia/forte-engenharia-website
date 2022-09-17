76c051b72a03a8ea6b537c80bbe14a56e418e801
Douglas Junior <douglasrtl45@gmail.com>
2022-09-17 17:56:36 -0300

    font ajustment and add components

--- src/js/slider.js
+++ src/js/slider.js
@@ -5,18 +5,16 @@ const $dualCarousel = document.querySelector('.js-carousel--dual');
 new Glider($dualCarousel,{
   slidesToShow:3,
   slidesToScroll:2,
+  scrollLock: true,
   draggable:true,
-  dots: '.js-carousel--dual-dots',
+  dots:'.js-carousel--dual-dots',
   arrows: {
     prev:".js-carousel--dual-prev",
     next:".js-carousel--dual-next",
-  },
-  scrollLock: true,
+  }
 });
 
-//simples slider
-
-// Dual slider
+//Simple slider
 
 const $simpleCarousel = document.querySelector('.js-carousel--simple');
 
@@ -25,8 +23,8 @@ new Glider($simpleCarousel,{
   slidesToScroll:1,
   draggable:true,
   dragVelocity:2,
-  dots: '.js-carousel--simple-dots',
-  arrows: {
+  dots:'.js-carousel--simple-dots',
+  arrows:{
     prev:".js-carousel--simple-prev",
     next:".js-carousel--simple-next",
   },
