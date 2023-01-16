c31a8ed0d3bc7203632a40b6f0b4714257a289fa
Douglas Junior <douglasrtl@users.noreply.github.com>
2023-01-16 13:08:22 -0300

    new journey

--- docs/js/journey.js
+++ docs/js/journey.js
@@ -3,30 +3,13 @@
 const $journeyCarousel = document.querySelector('.js-carousel--story')
 
 new Glider($journeyCarousel,{
-  slidesToShow:2,
-  slidesToScroll:2,
-  draggable:true,
-  dragVelocity:2,
+  slidesToShow:1,
+  slidesToScroll:1,
   dots:'.js-carousel--story-dots',
   arrows:{
     prev:".js-carousel--story-prev",
     next:".js-carousel--story-next",
   },
   scrollLock: true,
-  responsive: [ 
-    {
-      breakpoint: 910,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
-        itemWidth: 200,
-      }
-    },{
-      breakpoint: 925,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 2,
-      }
-    }
-  ]
 });
+
